83d90f8daae65ebdf2245c478e6464aeb1d77e05
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-08-20 22:18:45 +0100

    add positioning models

---
+++ Far/Far.Library/Models/CharPosition.cs
@@ -0,0 +1,92 @@
+﻿/*
+     Copyright 2024 Alastair Lundy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+namespace Far.Library.Models;
+
+public class CharPosition : IEquatable<CharPosition>
+{
+    public CharPosition()
+    {
+        LineNumber = 0;
+        ColumnNumber = 0;
+    }
+
+    public CharPosition(int lineNumber, int columnNumber)
+    {
+        this.LineNumber = lineNumber;
+        this.ColumnNumber = columnNumber;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int LineNumber { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int ColumnNumber { get; set; }
+
+    /// <summary>
+    /// Determines whether the object provided is equal to the current object.
+    /// </summary>
+    /// <param name="obj">The object to be compared.</param>
+    /// <returns>true if the object provided is equal to the current object; false otherwise.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+        
+        if (obj is CharPosition position)
+        {
+            return Equals(position);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Returns the hashcode for this object.
+    /// </summary>
+    /// <returns>A 32-Bit signed integer hashcode.</returns>
+    public override int GetHashCode()
+    {
+        string hash = $"LN{LineNumber}CN{ColumnNumber}";
+        
+        return hash.GetHashCode();
+    }
+
+    /// <summary>
+    /// Determines whether the TextCaretPosition provided is equal to the current object.
+    /// </summary>
+    /// <param name="other">The object to be compared.</param>
+    /// <returns>true if the object provided is equal to the current object; false otherwise.</returns>
+    public bool Equals(CharPosition? other)
+    {
+        if (other is not null)
+        {
+            return (this.LineNumber == other.LineNumber) && (this.ColumnNumber == other.ColumnNumber);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Far/Far.Library/Models/StringPosition.cs
@@ -0,0 +1,79 @@
+﻿/*
+     Copyright 2024 Alastair Lundy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+namespace Far.Library.Models;
+
+public class StringPosition : IEquatable<StringPosition>
+{
+    public StringPosition()
+    {
+        StartPosition = new CharPosition();
+        EndPosition = new CharPosition();
+    }
+
+    public StringPosition(CharPosition startPosition, CharPosition endPosition)
+    {
+        StartPosition = startPosition;
+        EndPosition = endPosition;
+    }
+    
+    public CharPosition StartPosition { get; set; }
+    public CharPosition EndPosition { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(StringPosition? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+        else
+        {
+            return StartPosition.Equals(other.StartPosition) && EndPosition.Equals(other.EndPosition);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj is StringPosition position)
+        {
+            return Equals(position);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(StartPosition.GetHashCode(), EndPosition.GetHashCode());
+    }
+}

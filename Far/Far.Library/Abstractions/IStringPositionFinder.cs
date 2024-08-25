38503cb246aab176c897960f6988a2c3023b9d53
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-08-25 13:54:52 +0100

    Far: add interfaces

---
+++ Far/Far.Library/Abstractions/IStringPositionFinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using Far.Library.Models;
+
+namespace Far.Library.Abstractions;
+
+public interface IStringPositionFinder
+{
+    public IEnumerable<CharPosition> GetCharPositions(string toBeSearched, char expected, bool ignoreCase);
+    public IEnumerable<CharPosition> GetCharPositions(IEnumerable<string> strings, char expected, bool ignoreCase);
+
+    public IEnumerable<StringPosition> GetStringPositions(string stringToBeSearched, string expected, bool ignoreCase);
+
+    public IEnumerable<StringPosition> GetStringPositions(IEnumerable<string> strings, string expected, bool ignoreCase);
+
+}
---
+++ Far/Far.Library/Abstractions/IStringReplacer.cs
@@ -0,0 +1,36 @@
+﻿/*
+     Copyright 2024 Alastair Lundy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System.Collections.Generic;
+
+namespace Far.Library.Abstractions;
+
+public interface IStringReplacer
+{
+    public string ReplaceCharacter(string original, char toBeReplaced, char replacementChar);
+
+    public IEnumerable<string> ReplaceCharacter(IEnumerable<string> enumerable, char toBeReplaced, char replacementChar);
+
+    public string ReplaceExactMatch(string original, string toBeReplaced, string replacementString);
+
+    public IEnumerable<string> ReplaceExactMatch(IEnumerable<string> enumerable, string toBeReplaced,
+        string replacementString);
+
+    public string ReplacePartialMatch(string original, string toBeReplaced, string replacementString);
+
+    public IEnumerable<string> ReplacePartialMatch(IEnumerable<string> enumerable, string toBeReplaced,
+        string replacementString);
+}

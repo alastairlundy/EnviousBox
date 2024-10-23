105f7c2ba4fa15b668c26fa298e4aecef5b1bfd3
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-23 14:41:56 +0100

    move work in progress Far commands to envious cli

---
+++ EnviousBox/EnviousBox.Cli/Tools/Improve/Commands/UpdateAll.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console.Cli;
+
+namespace EnviousBox.Cli.Tools.Improve.Commands;
+
+public class UpdateAll : Command<UpdateAll.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        throw new System.NotImplementedException();
+    }
+}

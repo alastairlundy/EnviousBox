edd1517c0de7c202c789869c2f76740bb9b4e4dc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-25 14:45:14 +0000

    update copyright notice

--- EnviousBox.Libraries/CaesarLib/CaesarCipher.cs
+++ EnviousBox.Libraries/CaesarLib/CaesarCipher.cs
@@ -1,6 +1,6 @@
 ﻿/*
     EnviousBox - Caesar Library
-    Copyright (C) 2024 Alastair Lundy
+    Copyright (C) 2024-2025 Alastair Lundy
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
